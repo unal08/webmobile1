1b088f71cb8a1344bf243183ea6d2e35ef03b21f
unal08 <63405412+unal08@users.noreply.github.com>
2023-10-15 18:40:44 +0400

    Add files via upload

---
+++ node.js
@@ -0,0 +1,20 @@
+var searchbar = document.getElementById('search-toggle');
+
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    var menu = document.getElementById('searchbar');
+    menu.classList.toggle('is-open');
+});
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+
+  
+
+ 
